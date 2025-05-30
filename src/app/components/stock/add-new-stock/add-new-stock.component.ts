33fb9923cea9e6db1d939e12a7b0cb82b4d9451f
Kasim Sabir <41677739+Qasim223@users.noreply.github.com>
2025-05-30 18:20:50 +0100

    new stock components

---
+++ src/app/components/stock/add-new-stock/add-new-stock.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-stock',
+  imports: [],
+  templateUrl: './add-new-stock.component.html',
+  styleUrl: './add-new-stock.component.css'
+})
+export class AddNewStockComponent {
+
+}
---
+++ src/app/components/stock/stock-control/stock-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-control',
+  imports: [],
+  templateUrl: './stock-control.component.html',
+  styleUrl: './stock-control.component.css'
+})
+export class StockControlComponent {
+
+}
