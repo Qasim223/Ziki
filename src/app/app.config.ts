fba6b3e73de531c3fde0979dea3ed8957e63bc46
Kasim Sabir <41677739+Qasim223@users.noreply.github.com>
2025-05-31 16:34:11 +0100

    custom preset added | Broke dark mode toggle

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,10 +10,7 @@ import {
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import Lara from '@primeng/themes/lara';
-import Material from '@primeng/themes/material';
-import Nora from '@primeng/themes/nora';
+import MyPreset from '../preset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,10 +21,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
           prefix: 'p',
-          darkModeSelector: '.my-app-dark',
+          darkModeSelector: false || 'none',
           cssLayer: {
             name: 'primeng',
             order: 'theme, base, primeng',
