01d68e54e7a66fbf710b50f84d5c902d63a2d1d7
Kasim Sabir <41677739+Qasim223@users.noreply.github.com>
2025-06-02 16:51:29 +0100

    new Sales component added

---
+++ src/app/components/sales/sales-control/sales-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-control',
+  imports: [],
+  templateUrl: './sales-control.component.html',
+  styleUrl: './sales-control.component.css'
+})
+export class SalesControlComponent {
+
+}
